aa8799976ddff1788ff96e2b143d07271bbca92a
Aleksandr Rubanenko <arubanenko@qualium-systems.com.ua>
2018-05-04 12:50:51 +0300

    .

--- date-input-polyfill.js
+++ date-input-polyfill.js
@@ -4,9 +4,9 @@ import Input from './input.js';
 const addPickers = () => {
   Input.addPickerToOtherInputs();
   // Check if type="date" is supported.
-  // if(!Input.supportsDateInput()) {
+  if(!Input.supportsDateInput()) {
     Input.addPickerToDateInputs();
-  // }
+  }
 };
 
 // Run the above code on any <input type="date"> in the document, also on dynamically created ones.
